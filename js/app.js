b689060fa5a6800e2353fb7b1146eb7f70bd5760
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-06 22:21:33 -0700

    Added Default name to user and checks for failing the game.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var counter = 0;
-var nameOfUser = 'noNameProvided';
+var score = 0;
+var nameOfUser = 'Friend';
 
 function isGameOver(){
   var count = 0;
@@ -12,29 +12,31 @@ function isGameOver(){
       count += 1;
     }
   }
-  return count === formsCount;
+  return count === formsCount || score <= -formsCount;
 }
 
 // This function is going to be called from HTML
 function guessingGame(form) { // eslint-disable-line no-unused-vars
   if (form.userResponse.value === form.answer.value) {
-    counter += 1;
-    var points1 = 'You have ' + counter + ' point(s).';
+    score += 1;
+    var points1 = 'You have ' + score + ' point(s).';
 
     alert('You got it right! ' + points1);
   } else {
-    counter += -1;
-    var points2 = 'You have ' + counter + ' point(s).';
+    score += -1;
+    var points2 = 'You have ' + score + ' point(s).';
     alert('Sorry! Try again. ' + points2);
   }
   console.log(nameOfUser + ' answered: ' + form.userResponse.value);
 
   if (isGameOver()){
-    alert('Thankyou for playing ' + nameOfUser + '! Final score: ' + counter);
+    alert('Thank you for playing ' + nameOfUser + '! Final score: ' + score);
   }
 }
 
 window.onload = function() {
-  nameOfUser = prompt( 'Welcome to my page. Please tell me your name:');
+  var welcome = 'Welcome to my page. Please tell me your name:';
+  var getName = prompt(welcome);
+  nameOfUser = getName ? getName : nameOfUser;
   alert('Hi ' + nameOfUser + '! Please answer some questions to play my game.');
 };
