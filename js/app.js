585f973023902ea7651359774c80778d8846777d
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-06 22:09:11 -0700

    Added a game ending function.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@
 var counter = 0;
 var nameOfUser = 'noNameProvided';
 
+function isGameOver(){
+  var count = 0;
+  var elements  = document.getElementsByTagName('input');
+  var formsCount = document.getElementsByTagName('form').length;
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].type == 'radio' && elements[i].checked){
+      count += 1;
+    }
+  }
+  return count === formsCount;
+}
+
 // This function is going to be called from HTML
 function guessingGame(form) { // eslint-disable-line no-unused-vars
   if (form.userResponse.value === form.answer.value) {
@@ -15,7 +27,11 @@ function guessingGame(form) { // eslint-disable-line no-unused-vars
     var points2 = 'You have ' + counter + ' point(s).';
     alert('Sorry! Try again. ' + points2);
   }
-  console.log('The user answered: ' + form.userResponse.value);
+  console.log(nameOfUser + ' answered: ' + form.userResponse.value);
+
+  if (isGameOver()){
+    alert('Thankyou for playing ' + nameOfUser + '! Final score: ' + counter);
+  }
 }
 
 window.onload = function() {
