9d06078bda16a3b43230520c0ce23adc92a5e941
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-08 12:58:00 -0700

    Added some color feedback to the Streach goals section.

--- stretchgoal/js/app.js
+++ stretchgoal/js/app.js
@@ -3,27 +3,41 @@
 var score = 0;
 var nameOfUser = 'Friend';
 
-function isGameOver(){
+function howManyChecked(form){
+  var elements =  form.getElementsByTagName('input');
   var count = 0;
-  var elements  = document.getElementsByTagName('input');
-  var formsCount = document.getElementsByTagName('form').length;
   for (var i = 0; i < elements.length; i++) {
     if (elements[i].type == 'radio' && elements[i].checked){
       count += 1;
     }
   }
-  return count === formsCount || score <= -formsCount;
+  return count;
+}
+
+function isGameOver(){
+  var formsCount = document.getElementsByTagName('form').length;
+  return howManyChecked(document) === formsCount || score <= -formsCount;
 }
 
 // This function is going to be called from HTML
 function guessingGame(form) { // eslint-disable-line no-unused-vars
+  if (form.nodeName !== 'FORM') { throw 'We were expecting a form here' ;}
+  var label =  form.getElementsByTagName('label')[0];
+
+  //Break out if the user did not check a box
+  if (!howManyChecked(form) > 0) {
+    return;
+  } else if (howManyChecked(form) === 1 && label.style.color === 'red' || label.style.color === 'green' ) {
+    return;
+  }
   if (form.userResponse.value === form.answer.value) {
     score += 1;
+    label.style.color = 'green';
     var points1 = 'You have ' + score + ' point(s).';
-
     alert('You got it right! ' + points1);
   } else {
     score += -1;
+    label.style.color = 'red';
     var points2 = 'You have ' + score + ' point(s).';
     alert('Sorry! Try again. ' + points2);
   }
